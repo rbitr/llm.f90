0cb5255773b5e780826a47f47faf84819f8f2da2
Andrew <andrewmarble@gmail.com>
2023-09-08 10:52:54 -0400

    bug in argparse

--- llama2.f90
+++ llama2.f90
@@ -110,6 +110,7 @@ module arg_parse
                                                 i = i + 1
                                                 case default
                                                 print *, 'Unrecognized option:', trim(arg)
+                                                i = i + 1
                                                 end select
                         end do
 
