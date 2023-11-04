40c3d6007e51058de0c970850ca2de26e92bdf46
Andrew <andrewmarble@gmail.com>
2023-11-04 11:29:27 -0400

    experimen ts with fp16 -> fp32 conversion

--- convert.c
+++ convert.c
@@ -1,9 +1,52 @@
-#include <fp16.h>
+//#include <fp16.h>
+#include <stdint.h>
 
 uint16_t float_to_half(float x) {
-	return fp16_ieee_from_fp32_value(x);
+	//return fp16_ieee_from_fp32_value(x);
+	return 0;
+}
+
+
+static inline float fp32_from_bits(uint32_t w) {
+    union {
+        uint32_t as_bits;
+        float as_value;
+    } fp32;
+    fp32.as_bits = w;
+    return fp32.as_value;
+}
+
+static inline uint32_t fp32_to_bits(float f) {
+    union {
+        float as_value;
+        uint32_t as_bits;
+    } fp32;
+    fp32.as_value = f;
+    return fp32.as_bits;
 }
 
 float half_to_float(uint16_t h) {
-	return fp16_ieee_to_fp32_value(h);
+    const uint32_t w = (uint32_t) h << 16;
+    const uint32_t sign = w & UINT32_C(0x80000000);
+    const uint32_t two_w = w + w;
+
+    const uint32_t exp_offset = UINT32_C(0xE0) << 23;
+#if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L) || defined(__GNUC__) && !defined(__STRICT_ANSI__)
+    const float exp_scale = 0x1.0p-112f;
+#else
+    const float exp_scale = fp32_from_bits(UINT32_C(0x7800000));
+#endif
+    const float normalized_value = fp32_from_bits((two_w >> 4) + exp_offset) * exp_scale;
+
+    const uint32_t magic_mask = UINT32_C(126) << 23;
+    const float magic_bias = 0.5f;
+    const float denormalized_value = fp32_from_bits((two_w >> 17) | magic_mask) - magic_bias;
+
+    const uint32_t denormalized_cutoff = UINT32_C(1) << 27;
+    const uint32_t result = sign |
+        (two_w < denormalized_cutoff ? fp32_to_bits(denormalized_value) : fp32_to_bits(normalized_value));
+    return fp32_from_bits(result);	
+	//return fp16_ieee_to_fp32_value(h);
 }
+
+
